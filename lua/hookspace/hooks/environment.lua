1948e76f643c7e53f2e36ff1607f157230279fad
midrare <midrare9@gmail.com>
2024-05-14 14:52:21 -0700

    feat: support environment variables in templates

--- lua/hookspace/hooks/environment.lua
+++ lua/hookspace/hooks/environment.lua
@@ -7,31 +7,131 @@ local user_opts = vim.deepcopy(default_opts)
 local old_env = nil
 
 local sep = vim.fn.has("win32") >= 1 and "\\" or "/"
+local pathsep = vim.fn.has("win32") >= 1 and ";" or ":"
 
-local function tbl_sub_all(tbl, pat, repl)
+local function is_env_varname_ok(varname)
+  return string.match(varname, "^[a-zA-Z_][a-zA-Z0-9_\\-]*$")
+end
+
+
+local function matchcmp(m1, m2)
+  if m1 == nil and m2 ~= nil then
+    return 1
+  end
+  if m1 ~= nil and m2 == nil then
+    return -1
+  end
+  if m1 == nil and m2 == nil then
+    return 0
+  end
+
+  if m1.start < m2.start or (m1.start <= m2.start
+    and (m1.stop - m1.start) < (m2.stop - m2.start)) then
+    return -1
+  end
+
+  if m2.start < m1.start or (m2.start <= m1.start
+    and (m2.stop - m2.start) < (m1.stop - m1.start)) then
+    return 1
+  end
+
+  return 0
+end
+
+local function replace_templates(s, workspace)
+  local repls = {
+    {
+      "%${[Ee][Nn][Vv]:[%a_][%a%d_ ]-}", function(m)
+        local name = m:match("%${[Ee][Nn][Vv]:(.*)}")
+        return os.getenv(name) or ""
+      end
+    },
+    { "%${[:;]+}", pathsep },
+    { "%${[\\/]+}", sep },
+    { "%${[Rr][Oo][Oo][Tt][Dd][Ii][Rr]}", workspace.rootdir() },
+    { "%${[Dd][Aa][Tt][Aa][Dd][Ii[Rr]}", workspace.datadir() },
+    { "%${[Ll][Oo][Cc][Aa][Ll[Dd][Ii][Rr]}", workspace.localdir() },
+  }
+
+  local init = 1
+  while init <= #s do
+    local best = nil
+
+    for _, p in ipairs(repls) do
+      local start, stop = s:find(p[1], init)
+      if start ~= nil and stop ~= nil then
+        local m = {
+          start = start,
+          stop = stop,
+          pat = p[1],
+          repl = p[2],
+        }
+        if matchcmp(best, m) > 0 then
+          best = m
+        end
+      end
+    end
+
+    if not best then
+      break
+    end
+
+    local repl = best.repl
+    if type(repl) == "function" then
+      repl = repl(s:sub(best.start, best.stop))
+    end
+
+    s = s:sub(1, best.start - 1) .. repl .. s:sub(best.stop + 1, #s)
+    init = best.start + #repl
+  end
+
+  return s
+end
+
+
+local function recursive_sub(tbl, workspace)
   for k, v in pairs(tbl) do
     if type(v) == "string" then
-      tbl[k] = v:gsub(pat, repl)
+      tbl[k] = replace_templates(tbl[k], workspace)
     elseif type(v) == "table" then
-      tbl_sub_all(v, pat, repl)
+      recursive_sub(v)
     end
   end
 end
 
-local function is_var_name_sane(varname)
-  return string.match(varname, "^[a-zA-Z0-9_\\-]*$")
+local function read_file(filename)
+  local fd = vim.loop.fs_open(filename, "r", 438)
+  if not fd then
+    return nil
+  end
+
+  local stat = vim.loop.fs_fstat(fd)
+  if not stat then
+    return nil
+  end
+
+  local data = vim.loop.fs_read(fd, stat.size, 0)
+  vim.loop.fs_close(fd)
+
+  return data
 end
 
-local function read_env_file(workspace, filename)
-  local plaintext = vim.fn.readfile(filename, "B")
+local function read_json(filename)
+  if vim.fn.filereadable(filename) <= 0 then
+    return nil
+  end
+
+  local plaintext = read_file(filename)
   if not plaintext then
     return nil
   end
-  local env = vim.fn.json_decode(plaintext)
-  tbl_sub_all(env, "${rootdir}", workspace.rootdir())
-  tbl_sub_all(env, "${datadir}", workspace.datadir())
-  tbl_sub_all(env, "${localdir}", workspace.localdir())
-  return env
+
+  local is_ok, o = pcall(vim.fn.json_decode, plaintext)
+  if not is_ok or not o then
+    return nil
+  end
+
+  return o
 end
 
 function M.setup(opts)
@@ -42,15 +142,14 @@ end
 function M.on_open(workspace)
   old_env = {}
 
-  local new_env = {}
-  local filename = workspace.localdir() .. sep .. "environment.json"
-  if vim.fn.filereadable(filename) >= 1 and user_opts.use_local then
-    local env = read_env_file(workspace, filename) or {}
-    new_env = vim.tbl_deep_extend("force", new_env, env)
-  end
+  local o1 = read_json(workspace.datadir() .. sep .. "environment.json") or {}
+  local o2 = read_json(workspace.localdir() .. sep .. "environment.json") or {}
+
+  local new_env = vim.tbl_deep_extend("force", {}, o1, o2)
+  recursive_sub(new_env, workspace)
 
   for name, value in pairs(new_env) do
-    if is_var_name_sane(name) then
+    if is_env_varname_ok(name) then
       old_env[name] = os.getenv(name) or false
       vim.fn.setenv(name, value or nil)
     end
@@ -61,7 +160,7 @@ end
 function M.on_close(workspace)
   if old_env then
     for name, value in pairs(old_env) do
-      if is_var_name_sane(name) then
+      if is_env_varname_ok(name) then
         vim.fn.setenv(name, value or nil)
       end
     end
--- lua/hookspace/hooks/lspconfig.lua
+++ lua/hookspace/hooks/lspconfig.lua
@@ -20,8 +20,95 @@ local default_opts = {}
 ---@diagnostic disable-next-line: unused-local
 local user_opts = vim.deepcopy(default_opts)
 
+local sep = vim.fn.has("win32") >= 1 and "\\" or "/"
+local pathsep = vim.fn.has("win32") >= 1 and ";" or ":"
+
 ---@type table?
 M.lsp_settings = nil
+
+local function matchcmp(m1, m2)
+  if m1 == nil and m2 ~= nil then
+    return 1
+  end
+  if m1 ~= nil and m2 == nil then
+    return -1
+  end
+  if m1 == nil and m2 == nil then
+    return 0
+  end
+
+  if m1.start < m2.start or (m1.start <= m2.start
+    and (m1.stop - m1.start) < (m2.stop - m2.start)) then
+    return -1
+  end
+
+  if m2.start < m1.start or (m2.start <= m1.start
+    and (m2.stop - m2.start) < (m1.stop - m1.start)) then
+    return 1
+  end
+
+  return 0
+end
+
+local function replace_templates(s, workspace)
+  local repls = {
+    {
+      "%${[Ee][Nn][Vv]:[%a_][%a%d_ ]-}", function(m)
+        local name = m:match("%${[Ee][Nn][Vv]:(.*)}")
+        return os.getenv(name) or ""
+      end
+    },
+    { "%${[:;]+}", pathsep },
+    { "%${[\\/]+}", sep },
+    { "%${[Rr][Oo][Oo][Tt][Dd][Ii][Rr]}", workspace.rootdir() },
+    { "%${[Dd][Aa][Tt][Aa][Dd][Ii[Rr]}", workspace.datadir() },
+    { "%${[Ll][Oo][Cc][Aa][Ll[Dd][Ii][Rr]}", workspace.localdir() },
+  }
+
+  local init = 1
+  while init <= #s do
+    local best = nil
+
+    for _, p in ipairs(repls) do
+      local start, stop = s:find(p[1], init)
+      if start ~= nil and stop ~= nil then
+        local m = {
+          start = start,
+          stop = stop,
+          pat = p[1],
+          repl = p[2],
+        }
+        if matchcmp(best, m) > 0 then
+          best = m
+        end
+      end
+    end
+
+    if not best then
+      break
+    end
+
+    local repl = best.repl
+    if type(repl) == "function" then
+      repl = repl(s:sub(best.start, best.stop))
+    end
+
+    s = s:sub(1, best.start - 1) .. repl .. s:sub(best.stop + 1, #s)
+    init = best.start + #repl
+  end
+
+  return s
+end
+
+local function recursive_sub(tbl, workspace)
+  for k, v in pairs(tbl) do
+    if type(v) == "string" then
+      tbl[k] = replace_templates(tbl[k], workspace)
+    elseif type(v) == "table" then
+      recursive_sub(v)
+    end
+  end
+end
 
 local function read_file(filename)
   local fd = vim.loop.fs_open(filename, "r", 438)
@@ -58,30 +145,14 @@ local function read_json(filename)
   return o
 end
 
-local function recursive_sub(tbl, pat, repl)
-  for k, v in pairs(tbl) do
-    if type(v) == "string" then
-      tbl[k] = v:gsub(pat, repl)
-    elseif type(v) == "table" then
-      recursive_sub(v, pat, repl)
-    end
-  end
-end
-
-local function replace_templates(o, workspace)
-  recursive_sub(o, "${rootdir}", workspace.rootdir())
-  recursive_sub(o, "${datadir}", workspace.datadir())
-  recursive_sub(o, "${localdir}", workspace.localdir())
-end
-
 local function read_configs(workspace)
   local config = {}
 
-  local o1 = read_json(workspace.datadir() .. "/" .. "lspconfig.json") or {}
-  local o2 = read_json(workspace.localdir() .. "/" .. "lspconfig.json") or {}
+  local o1 = read_json(workspace.datadir() .. sep .. "lspconfig.json") or {}
+  local o2 = read_json(workspace.localdir() .. sep .. "lspconfig.json") or {}
 
   config = vim.tbl_deep_extend("force", config, o1, o2)
-  replace_templates(config, workspace)
+  recursive_sub(config, workspace)
 
   return config
 end
